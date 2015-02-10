ec7ea7385654ac85d935bc4238e4a703e3f5cef6
Paul <paul.irwin90@gmail.com>
2015-02-10 02:02:46 -0700

    Initial Commit

---
+++ OpenCV Tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV Tutorial
+//
+//  Created by Paul Irwin on 2/10/15.
+//  Copyright (c) 2015 Paul Irwin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
